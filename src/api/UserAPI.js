ae5ec3c70f73053f323639e0644bd97b5cb9264c
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-05-08 22:33:20 -0600

    switched api call to heroku backend

--- src/api/UserAPI.js
+++ src/api/UserAPI.js
@@ -1,6 +1,6 @@
 
 const getLoggedInUser = (token) => {
-  return fetch('http://localhost:8000/core/current_user/', {
+  return fetch('https://moonbase-backend.herokuapp.com/core/current_user/', {
     headers: {
       'Content-Type': 'application/json',
       'Authorization': `JWT ${token}`
@@ -9,7 +9,7 @@ const getLoggedInUser = (token) => {
 };
 
 const signupUser = (userObject) => {
-  return fetch('http://localhost:8000/core/users/', {
+  return fetch('https://moonbase-backend.herokuapp.com/core/users/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -19,7 +19,7 @@ const signupUser = (userObject) => {
 };
 
 const login = (userObject) => {
-  return fetch('http://localhost:8000/token-auth/', {
+  return fetch('https://moonbase-backend.herokuapp.com/token-auth/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -32,7 +32,7 @@ const login = (userObject) => {
 // get favorites on user account db
 const fetchFavorites = async (token) => {
   try {
-    let response = await fetch('http://localhost:8000/core/current_user', {
+    let response = await fetch('https://moonbase-backend.herokuapp.com/core/current_user', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
@@ -52,7 +52,7 @@ const fetchFavorites = async (token) => {
 
 //post favorites to account http://localhost:8000/core/favorites/
 const addToFavoritesDb = async (token, videoObject) => {
-  return fetch('http://localhost:8000/core/favorites/', {
+  return fetch('https://moonbase-backend.herokuapp.com/core/favorites/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -65,7 +65,7 @@ const addToFavoritesDb = async (token, videoObject) => {
 
 //delete favorite from account http://localhost:8000/core/favorites/
 const deleteFavorite = async (token, favoriteId) => {
-  return fetch(`http://localhost:8000/core/favorites/${favoriteId}`, {
+  return fetch(`https://moonbase-backend.herokuapp.com/core/favorites/${favoriteId}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
